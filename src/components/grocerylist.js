1fede6751763a2b66632d8c20f797963c45db7c2
Aveiro746 <95100461+Aveiro746@users.noreply.github.com>
2022-02-21 19:13:32 -0500

    Grocery_react_list

---
+++ src/components/grocerylist.js
@@ -0,0 +1,15 @@
+const GroceryList = () => {
+    state = {
+        item: '',
+        brand: '',
+        units: '',
+        quantity: 0,
+        isPurchased: false
+    }
+    return(
+        
+        <h1>GroceryList </h1>
+        
+    )
+    
+}
